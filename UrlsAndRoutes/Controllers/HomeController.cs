197a1198ce3dabd9dcf52a08f16023b76441c66e
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-12 17:06:38 +0300

    Defining Optional URL Segments

--- UrlsAndRoutes/Controllers/HomeController.cs
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace UrlsAndRoutes.Controllers
         public ViewResult CustomVariable(string id)
         {
             Result r = new Result { Controller = nameof(HomeController), Action = nameof(CustomVariable), };
-            r.Data["Id"] = id;
+            r.Data["Id"] = id ?? "<no value>";
 
             return View("Result", r);
         }
